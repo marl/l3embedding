d2fd45640128d9372e135f47575cbd7300f83664
Jason Cramer <jason.t.cramer@gmail.com>
2019-02-22 01:37:44 -0500

    Replace minispec with kapre v0.1.4

--- l3embedding/train.py
+++ l3embedding/train.py
@@ -7,7 +7,6 @@ import pickle
 import random
 import csv
 
-import minispec
 import numpy as np
 import keras
 from keras.optimizers import Adam
@@ -141,7 +140,7 @@ def cycle_shuffle(iterable, shuffle=True):
 
 
 def data_generator(data_dir, batch_size=512, random_state=20180123,
-                   start_batch_idx=None, keys=None, model_type='cnn_L3_melspec2'):
+                   start_batch_idx=None, keys=None):
     random.seed(random_state)
 
     batch = None
@@ -187,45 +186,7 @@ def data_generator(data_dir, batch_size=512, random_state=20180123,
                     batch['video'] = 2 * img_as_float(batch['video']).astype('float32') - 1
 
                     # Convert audio to float
-                    audio_data_batch = batch['audio']
-                    batch['audio'] = []
-
-                    for audio_data in audio_data_batch:
-                        audio_data = pcm2float(audio_data.flatten(), dtype='float32')
-                        # Compute spectrogram
-                        if model_type == 'cnn_L3_melspec2':
-                            S = np.abs(minispec.core.stft(audio_data, n_fft=2048, hop_length=242,
-                                                          window='hann', center=True,
-                                                          pad_mode='constant'))
-                            audio_data = minispec.feature.melspectrogram(sr=48000, S=S,
-                                                                         n_mels=256, power=1.0,
-                                                                         htk=True)
-                            del S
-                        elif model_type == 'cnn_L3_melspec1':
-                            S = np.abs(minispec.core.stft(audio_data, n_fft=2048, hop_length=242,
-                                                          window='hann', center=True,
-                                                          pad_mode='constant'))
-                            audio_data = minispec.feature.melspectrogram(sr=48000, S=S,
-                                                                         n_mels=128, power=1.0,
-                                                                         htk=True)
-                            del S
-                        else:
-
-                            audio_data = np.abs(minispec.core.stft(audio_data, n_fft=512, hop_length=242,
-                                                                   window='hann', center=True,
-                                                                   pad_mode='constant'))
-
-                        # Convert amplitude to dB
-                        audio_data = minispec.core.amplitude_to_db(audio_data)
-
-                        # Add additional batch and channel dimensions
-                        batch['audio'].append(audio_data[np.newaxis,:,:,np.newaxis])
-
-                        del audio_data
-
-                    del audio_data_batch
-                    batch['audio'] = np.vstack(batch['audio']).astype('float32')
-
+                    batch['audio'] = pcm2float(batch['audio'], dtype='float32')
 
                     yield batch
 
@@ -416,8 +377,7 @@ def train(train_data_dir, validation_data_dir, output_dir,
         train_data_dir,
         batch_size=train_batch_size,
         random_state=random_state,
-        start_batch_idx=train_start_batch_idx,
-        model_type=model_type)
+        start_batch_idx=train_start_batch_idx)
 
     train_gen = pescador.maps.keras_tuples(train_gen,
                                            ['video', 'audio'],
@@ -428,8 +388,7 @@ def train(train_data_dir, validation_data_dir, output_dir,
         validation_data_dir,
         validation_epoch_size,
         batch_size=validation_batch_size,
-        random_state=random_state,
-        model_type=model_type)
+        random_state=random_state)
 
     val_gen = pescador.maps.keras_tuples(val_gen,
                                          ['video', 'audio'],
